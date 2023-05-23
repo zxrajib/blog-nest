1849ecee0e924aff1fac1b18fc41fce71edfc1ff
Rajib <zxrajib@gmail.com>
2023-05-23 15:28:59 +0600

    Swagger add

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -6,7 +7,14 @@ async function bootstrap() {
   const host = process.env.SYSTEM_HOST;
   const app = await NestFactory.create(AppModule);
 
-
+  const config = new DocumentBuilder()
+  .setTitle('Blog API')
+  .setDescription('The cats API description')
+  .setVersion('1.0')
+  .addTag('Blog')
+  .build();
+const document = SwaggerModule.createDocument(app, config);
+SwaggerModule.setup('api', app, document)
 
   await app.listen(port);
  
